c78a4a2d70e8302fa9a6aaf10054bf5bc85c0a9f
Alejandbel <bogdanov28c@gmail.com>
2025-04-30 11:08:06 +0300

    feat: build only tact contracts

--- template/jest.setup.ts
+++ template/jest.setup.ts
@@ -1,6 +1,6 @@
-import { buildAll } from '@ton/blueprint';
+import { buildAllTact } from '@ton/blueprint';
 
 export default async function () {
-    console.log('\n\nBuild all contracts...\n\n');
-    await buildAll();
+    console.log('\n\nBuilding tact contracts...\n\n');
+    await buildAllTact();
 }
