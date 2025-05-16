6a44653e5f00f3ab94dd439dd4d7fa6232f27c36
Ilyar <ilyar.software@gmail.com>
2025-05-16 15:17:13 +0200

    feat: update jest config in template for support gas report

--- template/jest.config.ts
+++ template/jest.config.ts
@@ -3,8 +3,13 @@ import type { Config } from 'jest';
 const config: Config = {
     preset: 'ts-jest',
     globalSetup: './jest.setup.ts',
-    testEnvironment: 'node',
+    testEnvironment: '@ton/sandbox/jest-environment',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    reporters: [
+        'default',
+        ['@ton/sandbox/jest-reporter', {
+        }],
+    ]
 };
 
 export default config;
