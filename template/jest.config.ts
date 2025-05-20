be447e565ac62685dd5aa78e21a7280a2d869e09
ilyar <761285+ilyar@users.noreply.github.com>
2025-05-20 19:49:00 +0200

    feat: update jest config in template to support gas report (#33)

--- template/jest.config.ts
+++ template/jest.config.ts
@@ -4,8 +4,13 @@ const config: Config = {
     preset: 'ts-jest',
     globalSetup: './jest.setup.ts',
     cache: false, // disabled caching to prevent old Tact files from being used after a rebuild
-    testEnvironment: 'node',
+    testEnvironment: '@ton/sandbox/jest-environment',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    reporters: [
+        'default',
+        ['@ton/sandbox/jest-reporter', {
+        }],
+    ]
 };
 
 export default config;
