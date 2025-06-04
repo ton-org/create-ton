bf039b7c2b2dc7532ff9ecaca30118bc54da3b42
ilyar <761285+ilyar@users.noreply.github.com>
2025-06-04 18:48:56 +0200

    chore: setup linting (#36)

--- template/jest.config.ts
+++ template/jest.config.ts
@@ -6,11 +6,7 @@ const config: Config = {
     cache: false, // disabled caching to prevent old Tact files from being used after a rebuild
     testEnvironment: '@ton/sandbox/jest-environment',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-    reporters: [
-        'default',
-        ['@ton/sandbox/jest-reporter', {
-        }],
-    ]
+    reporters: ['default', ['@ton/sandbox/jest-reporter', {}]],
 };
 
 export default config;
